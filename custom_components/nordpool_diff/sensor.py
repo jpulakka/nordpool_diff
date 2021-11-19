e1db2f5b192d6f0dc493e25217752b8bec5e41cd
Joonas Pulakka <joonas.pulakka@iki.fi>
2021-11-19 16:11:08 +0200

    Triangle filter prototype

--- custom_components/nordpool_diff/sensor.py
+++ custom_components/nordpool_diff/sensor.py
@@ -9,13 +9,17 @@ from homeassistant.util import dt
 
 NORDPOOL_ENTITY = "nordpool_entity"
 FILTER_LENGTH = "filter_length"
+FILTER_TYPE = "filter_type"
+RECTANGLE = "rectangle"
+TRIANGLE = "triangle"
 
 # https://developers.home-assistant.io/docs/development_validation/
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(NORDPOOL_ENTITY): str,
     # TODO add sanity checks for filter-length, maybe optional with good default?
-    vol.Required(FILTER_LENGTH): vol.Coerce(int)
+    vol.Required(FILTER_LENGTH): vol.Coerce(int),
     # TODO add optional name, adjustable unit
+    vol.Optional(FILTER_TYPE, default=RECTANGLE): vol.In([RECTANGLE, TRIANGLE])
 })
 
 
@@ -27,17 +31,24 @@ def setup_platform(
 ) -> None:
     nordpool_entity_id = config[NORDPOOL_ENTITY]
     filter_length = config[FILTER_LENGTH]
+    filter_type = config[FILTER_TYPE]
 
-    add_entities([NordpoolDiffSensor(hass, nordpool_entity_id, filter_length)])
+    add_entities([NordpoolDiffSensor(hass, nordpool_entity_id, filter_length, filter_type)])
 
 
 class NordpoolDiffSensor(SensorEntity):
 
-    def __init__(self, hass, nordpool_entity_id, filter_length):
+    def __init__(self, hass, nordpool_entity_id, filter_length, filter_type):
         self._state = None
         self._hass = hass
         self._nordpool_entity_id = nordpool_entity_id
-        self._filter = [-1] + [1 / (filter_length - 1)] * (filter_length - 1)
+        self._filter = [-1]
+        if filter_type == TRIANGLE:
+            triangular_number = (filter_length * (filter_length - 1)) / 2
+            for i in range(filter_length - 1, 0, -1):
+                self._filter += [i / triangular_number]
+        else:  # RECTANGLE
+            self._filter += [1 / (filter_length - 1)] * (filter_length - 1)
         self._name = f"nordpool_diff_{filter_length}"
 
     @property
