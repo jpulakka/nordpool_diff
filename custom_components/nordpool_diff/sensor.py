d5eb089d4e1dc5dba345882ed508707d39b6a02e
Joonas Pulakka <joonas.pulakka@iki.fi>
2021-11-20 14:43:10 +0200

    Adjustable name and unit, improved validation, unique id

--- custom_components/nordpool_diff/sensor.py
+++ custom_components/nordpool_diff/sensor.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import voluptuous as vol
+import homeassistant.helpers.config_validation as cv
 from homeassistant.components.sensor import PLATFORM_SCHEMA, SensorEntity
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
@@ -12,14 +13,16 @@ FILTER_LENGTH = "filter_length"
 FILTER_TYPE = "filter_type"
 RECTANGLE = "rectangle"
 TRIANGLE = "triangle"
+NAME = "name"
+UNIT = "unit"
 
 # https://developers.home-assistant.io/docs/development_validation/
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(NORDPOOL_ENTITY): str,
-    # TODO add sanity checks for filter-length, maybe optional with good default?
-    vol.Required(FILTER_LENGTH): vol.Coerce(int),
-    # TODO add optional name, adjustable unit
-    vol.Optional(FILTER_TYPE, default=RECTANGLE): vol.In([RECTANGLE, TRIANGLE])
+    vol.Required(NORDPOOL_ENTITY): cv.entity_id,
+    vol.Optional(FILTER_LENGTH, default=2): vol.All(vol.Coerce(int), vol.Range(min=2, max=15)),
+    vol.Optional(FILTER_TYPE, default=RECTANGLE): vol.In([RECTANGLE, TRIANGLE]),
+    vol.Optional(NAME, default=""): cv.string,
+    vol.Optional(UNIT, default="EUR/kWh/h"): cv.string
 })
 
 
@@ -32,13 +35,15 @@ def setup_platform(
     nordpool_entity_id = config[NORDPOOL_ENTITY]
     filter_length = config[FILTER_LENGTH]
     filter_type = config[FILTER_TYPE]
+    name = config[NAME]
+    unit = config[UNIT]
 
-    add_entities([NordpoolDiffSensor(hass, nordpool_entity_id, filter_length, filter_type)])
+    add_entities([NordpoolDiffSensor(hass, nordpool_entity_id, filter_length, filter_type, name, unit)])
 
 
 class NordpoolDiffSensor(SensorEntity):
 
-    def __init__(self, hass, nordpool_entity_id, filter_length, filter_type):
+    def __init__(self, hass, nordpool_entity_id, filter_length, filter_type, name, unit):
         self._state = None
         self._hass = hass
         self._nordpool_entity_id = nordpool_entity_id
@@ -49,11 +54,17 @@ class NordpoolDiffSensor(SensorEntity):
                 self._filter += [i / triangular_number]
         else:  # RECTANGLE
             self._filter += [1 / (filter_length - 1)] * (filter_length - 1)
-        self._name = f"nordpool_diff_{filter_length}"
+        self._unique = f"nordpool_diff_{filter_type}_{filter_length}"
+        self._name = name if name else self._unique
+        self._unit = unit
 
     @property
     def name(self) -> str:
         return self._name
+
+    @property
+    def unique_id(self):
+        return self._unique
 
     @property
     def icon(self) -> str:
@@ -65,7 +76,7 @@ class NordpoolDiffSensor(SensorEntity):
 
     @property
     def unit_of_measurement(self) -> str:
-        return "EUR/kWh/h"  # TODO this should depend on nordpool unit, or at least make adjustable
+        return self._unit
 
     def update(self) -> None:
         prices = self._get_next_n_hours(len(self._filter))
